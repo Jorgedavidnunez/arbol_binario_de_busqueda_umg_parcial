1bda49395100629758a90c288f318ba1daa3c320
jorgedavidnunez <dingox57@gmail.com>
2022-03-10 02:41:54 -0600

    primer_parcial

---
+++ Arbol_binario_parcial/src/modelo/main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author david
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frm_arbol_binario frm = new frm_arbol_binario();
+        frm.show();
+    }
+    
+}
